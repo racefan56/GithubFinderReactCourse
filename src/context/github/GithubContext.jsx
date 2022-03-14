fbb4c007332223c79db8c2a6ac7a8de603f6371b
Nicholas Smith <hamtorokaton@hotmail.com>
2022-03-14 11:51:23 -0500

    finished course section 11 and removed .env from tracking

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -3,45 +3,21 @@ import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //Get search results
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {});
-
-    const { items } = await response.json();
-
-    dispatch({ type: 'GET_USERS', payload: items });
-  };
-
-  const clearUsers = () => {
-    dispatch({ type: 'CLEAR_USERS'});
-  };
-
-  //Set loading
-  const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
+        ...state,
+        dispatch,
       }}
     >
       {children}
